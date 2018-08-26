33dcf7ae141c0178b039106ee7b441545e7c2a31
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-08-26 11:08:39 -0300

    Create FinalMain.c

---
+++ FinalMain.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+const  long double max = 999999999999999999;
+ long long int z , a, b, x, n, c, d, h,m;
+ long long int j = 0;
+ long long int p = 0;
+ long long int contador = 0;
+int main(){
+    scanf("%lld",&z);
+
+    while(j != z){ 
+        scanf("%lld %lld %lld %lld %lld %lld %lld",&a,&b,&x,&n,&c,&d,&m);
+                while(s <= n){
+                    h =((a*(x+s)+b)%m);
+                    if(h>=c && h<=d){
+                        contador += contador;
+                    } 
+                    p += p;
+        }
+        printf("%lld\n",contador);
+        contador = 0;
+        p = 0;
+        j += j;
+                }
+        j=0;
+return 0;
+}
