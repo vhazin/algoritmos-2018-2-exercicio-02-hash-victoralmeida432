5873be026f7472b963862ed5177d295b6a356187
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-08-26 15:21:03 -0300

    Update FinalMain.c

--- FinalMain.c
+++ FinalMain.c
@@ -2,28 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-const  long double max = 999999999999999999;
- long long int z , a, b, x, n, c, d, h,m;
- long long int j = 0;
- long long int p = 0;
- long long int contador = 0;
-int main(){
-    scanf("%lld",&z);
-
-    while(j != z){ 
-        scanf("%lld %lld %lld %lld %lld %lld %lld",&a,&b,&x,&n,&c,&d,&m);
-                while(s <= n){
-                    h =((a*(x+s)+b)%m);
-                    if(h>=c && h<=d){
-                        contador += contador;
-                    } 
-                    p += p;
+int main(void){
+    
+    long long i, r, cont = 0;
+    scanf("%lld", &i);
+    long long a, b, x, n, c, d, m, parcial = 0;
+    while (i--)
+    {
+        scanf("%lld %lld %lld %lld %lld %lld %lld", &a, &b, &x, &n, &c, &d, &m);
+        parcial = a * x + b;
+        r = parcial % m;
+        if (r >= c && r <= d)
+            cont++;
+        while (n--)
+        {
+            parcial = parcial + a;
+            if (parcial < m)
+                r = r + a;
+            else
+                r = parcial % m;
+            if (r >= c && r <= d)
+                cont++;
         }
-        printf("%lld\n",contador);
-        contador = 0;
-        p = 0;
-        j += j;
-                }
-        j=0;
-return 0;
+        printf("%lld\n", cont);
+        cont = 0;
+    }
+    return 0;
 }
